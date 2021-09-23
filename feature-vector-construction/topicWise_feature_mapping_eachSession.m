e8690b98b9393e67d12f73b315706dbde0899e54
WalterDai <71756048+ZhijunBioinf@users.noreply.github.com>
2021-09-23 23:42:06 +0800

    Add files via upload

---
+++ feature-vector-construction/topicWise_feature_mapping_eachSession.m
@@ -0,0 +1,85 @@
+function [topic_presence, audio_feature, video_feature, topic_id] = topicWise_feature_mapping_eachSession(unzip_dir, sessionID, topicsName, semantic_session_dir)
+
+num_topics = length(topicsName);
+topic_presence = -ones(num_topics,1);
+audio_feature = -ones(num_topics, 237); % (74 covarep + 5 formants) x 3(mean, max, min) = 237
+video_feature = -ones(num_topics, 60); % 20(action units) x 3(mean, max, min) = 60
+topic_id = [];
+
+covarep = load([unzip_dir filesep num2str(sessionID) '_COVAREP.csv']);
+formant = load([unzip_dir filesep num2str(sessionID) '_FORMANT.csv']);
+action_units = readtable([unzip_dir filesep num2str(sessionID) '_CLNF_AUs.txt']);
+action_units = table2array(action_units(:,5:end));
+
+transcript = readtable([unzip_dir filesep num2str(sessionID) '_TRANSCRIPT.csv']);
+row = size(transcript,1);
+i = 1;
+j = 1;
+while i <= row
+    if strcmp(transcript.speaker{i}, 'Ellie')
+        Ellie_words = strsplit(cell2mat(transcript.value(i)));
+        
+        for n = 1:num_topics
+            name_words = strsplit(cell2mat(topicsName(n)));
+            if length(Ellie_words)==1 && length(name_words)==1 && strcmp(Ellie_words, name_words) % for topic name: "why"
+                topic_record = n;
+                break
+            elseif all(ismember(name_words, Ellie_words))
+                topic_record = n;
+                break
+            else
+                topic_record = 0;
+            end
+        end
+        
+        if topic_record ~= 0
+            topic_id(j) = topic_record; j = j+1;
+            topic_presence(topic_record) = 1;
+            i = i+1;
+            while strcmp(transcript.speaker{i}, 'Ellie') && i<=row
+                i = i+1;
+            end
+            if i>row, break; end
+            subject_speech = [];
+            time_interval = [];
+            while strcmp(transcript.speaker{i}, 'Participant') && i<=row
+                subject_speech = [subject_speech transcript.value{i} ' '];
+                time_interval = [time_interval transcript.start_time(i) transcript.stop_time(i)];
+                i = i+1;
+            end
+            if i>row, break; end
+            
+            tmp_time_a = round(time_interval([1 end])*100);
+            tmp_time_v = round(time_interval([1 end])*30);
+            if tmp_time_a(2) > size(covarep,1) || tmp_time_a(2) > size(formant,1) || tmp_time_v(2) > size(action_units,1)
+                topic_presence(topic_record) = -1;
+                j = j-1; topic_id(j) = [];
+                warning('Files downloaded in SESSION #%d may be incomplete!!', sessionID)
+                break;
+            end
+            
+            semantic_file_name = sprintf('session-%d_topic-%d_speech.txt', sessionID, topic_record);
+            fid = fopen([semantic_session_dir filesep semantic_file_name], 'wt');
+            fprintf(fid, '%s\n', subject_speech);
+            fclose(fid);
+            
+            tmp = [covarep(tmp_time_a(1):tmp_time_a(2), :) formant(tmp_time_a(1):tmp_time_a(2), :)];
+            audio_feature(topic_record,:) = [mean(tmp) max(tmp) min(tmp)];
+            tmp = action_units(tmp_time_v(1):tmp_time_v(2), :);
+            video_feature(topic_record,:) = [mean(tmp) max(tmp) min(tmp)];
+        else
+            i = i+1;
+        end
+    else
+        i = i+1;
+    end
+end
+
+if j == 1
+    warning('No Ellie''s speech detected in SESSION #%d!!', sessionID)
+end
+
+
+
+
+
