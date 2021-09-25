2c7cd586227f537b0186371ae870df012bdfd916
WalterDai <71756048+ZhijunBioinf@users.noreply.github.com>
2021-09-25 19:57:02 +0800

    Update seqInvolve_rmRedun.m

--- two-stage-feature-selection/seqInvolve_rmRedun.m
+++ two-stage-feature-selection/seqInvolve_rmRedun.m
@@ -1,5 +1,16 @@
 function [cvEval_vec, finalFeature] = seqInvolve_rmRedun(train, sortInd, topPercent, kFoldCV, taskType, isDisplay)
 
+% Inputs:
+%   train: training set, a matrix where the data in the 1st column is Y and the remainder are X.
+%   sortInd: the feature ranking returned by a filter, such as 'mRMR_MIC'.
+%   topPercent: percent (if < 1) or the number of top features (if > 1) that should be scanned.
+%   kFoldCV: k-fold cross-validation (CV) used for obtaining a evaluation measure under a combination of hyper-parameters in a grid search process.
+%   taskType: a character vector indicating the type of learning task, 'classify' or 'regress'.
+%   isDisplay: if print program output, 1 do, 0 not.
+% Outputs:
+%   cvEval_vec: a vector indicating the evaluation measures corresponding to the selected features.
+%   finalFeature: indices of optimal feature subset.
+
 tr_Y = train(:,1);
 tr_X = train(:,2:end);
 x_num = size(tr_X,2);
@@ -32,10 +43,10 @@ count = 1;
 while count < topN
     if strcmp(taskType, 'classify')
         best_para = classifier([tr_Y tr_X(:,sortInd(1:m))], [], 0, kFoldCV, 1, ...
-            'libsvm', 'classify', ' -s 0 -t 2 -q ', 0);
+            'libsvm', 'classify', ' -s 0 -t 2 -q ', 0); % 'best_para = [cv_acc,bestC,bestG]', returned by a grid search for hyper-parameter tuning of a learner (e.g. SVM) in a cross-validation manner.
     elseif strcmp(taskType, 'regress')
         best_para = classifier([tr_Y tr_X(:,sortInd(1:m))], [], 0, kFoldCV, 1, ...
-            'libsvm', 'regress', ' -s 3 -t 2 -q ');
+            'libsvm', 'regress', ' -s 3 -t 2 -q '); % 'best_para = [cv_mse,bestC,bestG,bestP]'
     end
     cvEval = best_para(1);
     count = count + 1;
